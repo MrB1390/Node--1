399e96ba150caf4b65648f7b0e6df981c25c4706
MrB1390 <139531877+MrB1390@users.noreply.github.com>
2024-03-05 15:40:38 +0530

    updated the code

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 import express from "express";
 import fs from 'fs';
-import {format} from 'date-fns'
+import { format } from 'date-fns';
 
 const app = express();
 const PORT = 4000;
 
-const today = format(new Date(), 'dd-MM-yyyy-HH-mm-ss');
-const filePath = `TimeStamp/${today}.txt`;
-const content = `Current Date and Time: ${today}`;
+app.get('/',(req,res)=>{
+     res.status(200).send(`<div style="text-align: center">
+     <p> For create text file use  /write </p>
+     <p> For read text file use  /read </p>
+     </div>`)
+})
 
-app.get('/write', (req, res) => {
-  try {
-    fs.writeFileSync(filePath, content, "utf8");
-    res.status(200).send("File written Successfully");
-  } catch (error) {
-    res.status(500).send("Error writing File" + error.message);
-  }
-});
+app.get('/write', (req,res)=>{
+    const today = format(new Date(), 'dd-mm-yyyy-HH-mm');
+    const timeVal = format(new Date(), 'dd-mm-yyyy-HH-mm-ss');
+    const filePath = `TimeStamp/${today}.txt`
+    fs.writeFileSync(filePath, `Current Date and Time ${timeVal}` , 'utf8')
+    res.status(200).send("File written successfully")
+})
 
-app.get('/read', (req, res) => {
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    res.status(200).send(`${data}`);
-  } catch (error) {
-    res.status(500).send("Error reading File" + error.message);
-  }
-});
+app.get('/read',(req,res) => {
+  const today = format(new Date(), 'dd-mm-yyyy-HH-mm');
+  const filePath = `TimeStamp/${today}.txt`
+  let data = fs.readFileSync(filePath, 'utf-8')
+  res.status(200).send(data)
+})
+
 
 app.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
